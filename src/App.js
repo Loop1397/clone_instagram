bf06b4e26e853295f65e243846ed0e5c7acb7b37
Loop <shwlsdn0906@gmail.com>
2022-01-29 14:11:57 +0900

    react-create-app 및 전체 구역 나누기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./css/App.module.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <div className={styles.header__container}>헤더내부</div>
+            </header>
+            <div className={styles.container}>
+                <div className={styles.container__box}>
+                    <div className={styles.board__content}>왼</div>
+                    <div className={styles.side__menu}>오</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
