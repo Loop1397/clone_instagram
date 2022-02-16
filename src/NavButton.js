3196170bbdf758500cff4474d230e2ead0557cc2
Loop <shwlsdn0906@gmail.com>
2022-02-16 23:32:56 +0900

    네비게이션 버튼 눌렀을 때 색 바꾸기

---
+++ src/NavButton.js
@@ -0,0 +1,32 @@
+/**
+ * 입력 해야하는 요소
+ * 버튼 스타일(style)
+ * 이미지(imgStyle) or 텍스트(text)(둘중 하나만 들어감)
+ * url(url)
+ * +이미지 버튼일 시 이미지 파일 주소(image)
+ */
+
+function Button(prop) {
+    /**
+     * 버튼 클릭시 url 이동하는 메소드
+     */
+    const onClick = () => {
+        window.location.href = prop.url;
+        switch (prop.value) {
+            case 1:
+        }
+    };
+
+    const textBtn = () => {
+        return <span className={prop.textStyle}>{prop.text}</span>;
+    };
+    const imageBtn = () => {
+        return <img src={prop.image} className={prop.imgStyle} alt="image" />;
+    };
+    return (
+        <div className={prop.style} onClick={onClick}>
+            {prop.text ? textBtn() : imageBtn()}
+        </div>
+    );
+}
+export default Button;
--- src/Navigator.js
+++ src/Navigator.js
@@ -1,26 +1,33 @@
 import styles from "./css/Navigator.module.css";
 import Button from "./Button";
+import { useState } from "react";
 
 function Navigator() {
+    const [click, setClick] = useState([0, 0, 0, 0, 0, 0]);
+
+    const onClickNav = e => {
+        console.log(e.target.value);
+    };
+
     return (
         <div className={styles.nav__container}>
             <div>
-                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} image="/images/home.png" url="/" />
+                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onClick={url => onclick(url)} value={1} image="/images/home.png" url="/" />
             </div>
             <div>
-                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} image="/images/send.png" url="/direct/inbox/" />
+                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} value={2} image="/images/send.png" url="/direct/inbox/" />
             </div>
             <div>
-                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} image="/images/add.png" url="/create/select/" />
+                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} value={3} image="/images/add.png" url="/create/select/" />
             </div>
             <div>
-                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} image="/images/compass.png" url="/explore/" />
+                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} value={4} image="/images/compass.png" url="/explore/" />
             </div>
             <div>
-                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} image="/images/like.png" url="/" />
+                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} value={5} image="/images/like.png" url="/" />
             </div>
             <div>
-                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} image="/images/logo.png" url="/" />
+                <Button style={styles.nav__button} imgStyle={styles.nav__button__image} onclick={url => onclick(url)} value={6} image="/images/logo.png" url="/" />
             </div>
         </div>
     );
