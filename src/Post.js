41d896354e97c0e20f91fa454ff63251840a2d1b
Loop <shwlsdn0906@gmail.com>
2022-03-02 18:41:57 +0900

    Slide 개선, 애니메이션 추가해야함

--- src/Post.js
+++ src/Post.js
@@ -1,23 +1,43 @@
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 import Button from "./Button";
 import styles from "./css/Post.module.css";
 import Modal from "./Modal";
-import { Slide } from "react-slideshow-image";
+
+// import { Swiper } from "swiper";
+// import { Swiper, SwiperSlide } from "swiper/react";
+
+// import "swiper/swiper-bundle.min.css";
+// import "swiper/swiper.min.css";
+// import "swiper/components/navigation/navigation.min.css";
+
+// // import required modules
+// import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
 function Post() {
     const [isOpen, setIsOpen] = useState(false);
-    const [slide, setSlide] = useState(0);
+    const [slideIndex, setSlide] = useState(0);
 
     const slideImages = [styles.image1, styles.image2, styles.image3];
+    const slideStyles = [
+        {
+            position: "absolute",
+            right: "612px",
+        },
+        {
+            position: "absolute",
+            top: "0px",
+            left: "612px",
+        },
+    ];
 
     const leftSlide = () => {
-        if (0 < slide) {
+        if (0 < slideIndex) {
             setSlide(current => current - 1);
         }
     };
 
     const rightSlide = () => {
-        if (slide < slideImages.length - 1) {
+        if (slideIndex < slideImages.length - 1) {
             setSlide(current => current + 1);
         }
         console.log(slideImages.length);
@@ -32,6 +52,25 @@ function Post() {
         setIsOpen(false);
         // console.log("bye!");
         e.stopPropagation(); //이벤트 버블링을 막기 위한 메소드
+    };
+
+    const imageIndexRendering = () => {
+        const result = [];
+        // for (let i = 0; i < slideImages.length; i++) {
+        //     result.push(<span>{i}번째</span>);
+        // }
+
+        if (slideIndex !== 0) {
+            result.push(<div className={slideImages[slideIndex - 1]} style={slideStyles[0]}></div>);
+        }
+
+        result.push(<div className={slideImages[slideIndex]}></div>);
+
+        if (slideIndex !== slideImages.length) {
+            result.push(<div className={slideImages[slideIndex + 1]} style={slideStyles[1]}></div>);
+        }
+
+        return result;
     };
 
     return (
@@ -54,17 +93,23 @@ function Post() {
                 <div className={styles.container__image}>
                     <div className={styles.slide__button__left} onClick={leftSlide}></div>
                     <div className={styles.slide__button__right} onClick={rightSlide}></div>
-                    <div className={slideImages[slide]}></div>
+                    {imageIndexRendering()}
+
+                    {/* <Swiper
+                        className="swiper-container"
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        navigation
+                        pagination={{ clickable: true }}
+                        onSwiper={swiper => console.log(swiper)}
+                        onSlideChange={() => console.log("slide change")}
+                    >
+                        <SwiperSlide>slide 1</SwiperSlide>
+                        <SwiperSlide>slide 2</SwiperSlide>
+                        <SwiperSlide>slide 3</SwiperSlide>
+                    </Swiper> */}
                 </div>
-                {/* <div className={styles.each__slide}>
-                </div> */}
-                {/* <div className={styles.each__slide}>
-                    <div style={{ backgroundImage: `url(${slideImages[1]})` }}></div>
-                </div>
-                <div className={styles.each__slide}>
-                    <div style={{ backgroundImage: `url(${slideImages[2]})` }}></div>
-                </div> */}
-                <div className={styles.container__content}>게시글 내용</div>
+                <div className={styles.container__content}>하위</div>
                 <div className={styles.container__comment}>코멘트</div>
             </div>
         </div>
--- src/routes/Create.js
+++ src/routes/Create.js
@@ -1,11 +1,27 @@
 import Header from "../Header";
 
+// import { Swiper } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+// import required modules
+import { Navigation } from "swiper";
+
 function Create() {
     return (
         <section>
             <header>
                 <Header />
             </header>
+            <div>
+                <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                    <SwiperSlide>slide 1</SwiperSlide>
+                    <SwiperSlide>slide 2</SwiperSlide>
+                    <SwiperSlide>slide 3</SwiperSlide>
+                </Swiper>
+            </div>
         </section>
     );
 }
